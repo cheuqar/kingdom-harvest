7c9488982e20a73507625f45e78496326a32e56d
cheuqar <cheuqar@gmail.com>
2025-11-23 15:07:27 +0800

    Fix firebase export

--- app/src/config/firebase.js
+++ app/src/config/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAaWx7nrOtWxgNvoyKxaKw5LAuTaUtCqIA",
     authDomain: "kingdom-harvest-monopoly.firebaseapp.com",
@@ -20,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Realtime Database and export it
+export const db = getDatabase(app);
